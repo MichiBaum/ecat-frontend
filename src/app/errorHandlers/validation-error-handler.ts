decb7dcfac19300115687eaed0459c812b7de4bf
leon3131-ux <leon.domiter@edu.tbz.ch>
2020-04-06 13:32:45 +0200

    added injectable annotation

--- src/app/errorHandlers/validation-error-handler.ts
+++ src/app/errorHandlers/validation-error-handler.ts
@@ -1,6 +1,8 @@
 import {MessageService} from "primeng";
 import {HttpErrorResponse} from "@angular/common/http";
+import {Injectable} from "@angular/core";
 
+@Injectable()
 export class ValidationErrorHandler {
   constructor(private messageService: MessageService) {
   }
