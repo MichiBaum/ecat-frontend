fc24098cbdcd211605efaf7479614b33767bd26b
leon3131-ux <leon.domiter@edu.tbz.ch>
2020-04-06 13:32:06 +0200

    added injectable annotation

--- src/app/errorHandlers/auth-error-handler.ts
+++ src/app/errorHandlers/auth-error-handler.ts
@@ -1,7 +1,9 @@
 import {HttpResponseErrorHandler} from "./http-response-error-handler";
 import {HttpErrorResponse} from "@angular/common/http";
 import {MessageService} from "primeng";
+import {Injectable} from "@angular/core";
 
+@Injectable()
 export class AuthErrorHandler implements HttpResponseErrorHandler{
   constructor(private messageService: MessageService) {
   }
