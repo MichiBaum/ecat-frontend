1e1f8a01d3da143c0df35597306ae3140a11220f
leon3131-ux <leon.domiter@edu.tbz.ch>
2020-04-06 13:32:26 +0200

    added injectable annotation

--- src/app/errorHandlers/internal-server-error-handler.ts
+++ src/app/errorHandlers/internal-server-error-handler.ts
@@ -1,7 +1,9 @@
 import {MessageService} from "primeng";
 import {HttpErrorResponse} from "@angular/common/http";
 import {HttpResponseErrorHandler} from "./http-response-error-handler";
+import {Injectable} from "@angular/core";
 
+@Injectable()
 export class InternalServerErrorHandler implements HttpResponseErrorHandler{
   constructor(private messageService: MessageService) {
   }
