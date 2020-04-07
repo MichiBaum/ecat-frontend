3b9bca5bafabadcc3fa109791e2859d95bf8a203
leon3131-ux <leon.domiter@edu.tbz.ch>
2020-04-07 15:11:01 +0200

    added implements HttpResponseErrorHandler

--- src/app/errorHandlers/validation-error-handler.ts
+++ src/app/errorHandlers/validation-error-handler.ts
@@ -1,9 +1,10 @@
 import {MessageService} from "primeng";
 import {HttpErrorResponse} from "@angular/common/http";
 import {Injectable} from "@angular/core";
+import {HttpResponseErrorHandler} from "./http-response-error-handler";
 
 @Injectable()
-export class ValidationErrorHandler {
+export class ValidationErrorHandler implements HttpResponseErrorHandler{
 
   constructor(private messageService: MessageService) {}
 
