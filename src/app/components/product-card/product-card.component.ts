35d438b74660cfd647da0497bdefb2775bc36963
leon3131-ux <leon.domiter@edu.tbz.ch>
2020-05-22 10:45:17 +0200

    fixed performance issue on product-card

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,11 +1,10 @@
 import {
-  AfterViewChecked,
   Component, ElementRef,
   EventEmitter,
   HostBinding,
   Input,
   OnInit,
-  Output, ViewChild,
+  Output, Renderer2, ViewChild,
 } from '@angular/core';
 import {Product} from "../../models/product";
 import {MenuItem} from "primeng";
@@ -17,7 +16,7 @@ import {AuthenticationService} from "../../services/authentication.service";
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss'],
 })
-export class ProductCardComponent implements OnInit, AfterViewChecked {
+export class ProductCardComponent implements OnInit {
   @HostBinding('class.p-col-12') expandedClass = false;
   @HostBinding('class.p-sm-6') breakPointClassSm = true;
   @HostBinding('class.p-md-6') breakPointClassMd = true;
@@ -26,12 +25,12 @@ export class ProductCardComponent implements OnInit, AfterViewChecked {
   @Input() product: Product;
   @Output() deleteProduct = new EventEmitter();
   @ViewChild('container') containerElement: ElementRef;
+  @ViewChild('imageContainer') imageContainer: ElementRef;
   expanded: boolean = false;
-  containerWidth: number;
 
   productContextItems: MenuItem[];
 
-  constructor(private productService: ProductService, public authService: AuthenticationService) {
+  constructor(private productService: ProductService, public authService: AuthenticationService, private renderer2: Renderer2) {
   }
 
   ngOnInit(): void {
@@ -56,16 +55,13 @@ export class ProductCardComponent implements OnInit, AfterViewChecked {
     ];
   }
 
-  ngAfterViewChecked(): void {
-    setTimeout(() => {
-      this.containerWidth = this.containerElement.nativeElement.offsetWidth;
-    }, 0)
-  }
-
   expand(){
+    console.log('bruh');
     if(this.expanded == false){
+      this.renderer2.setStyle(this.imageContainer.nativeElement, 'width', this.containerElement.nativeElement.offsetWidth + 'px');
       this.expanded = true;
     }else{
+      this.renderer2.setStyle(this.imageContainer.nativeElement, 'width', '100%');
       this.expanded = false;
     }
     this.expandedClass = this.expanded;
