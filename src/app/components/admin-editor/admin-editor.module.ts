e34e124faeb8c7a5d611277487e6c79c4d37a843
leon3131-ux <leon.domiter@edu.tbz.ch>
2020-06-09 13:29:37 +0200

    refactoring

--- src/app/components/admin-editor/admin-editor.module.ts
+++ src/app/components/admin-editor/admin-editor.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AdminEditorComponent} from "./admin-editor.component";
-import {ButtonModule, DialogModule, DropdownModule, InputTextModule, MultiSelectModule, PasswordModule} from "primeng";
+import {ButtonModule, DialogModule, DropdownModule, InputTextModule, MultiSelectModule} from "primeng";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
