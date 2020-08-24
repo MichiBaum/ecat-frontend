8cab63e6cbf7fc9291d9195d145306c3b28423dc
leon3131-ux <leon.domiter@edu.tbz.ch>
2020-08-24 15:06:21 +0200

    updated url for navigation

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -104,7 +104,7 @@ export class NavigationComponent implements OnInit {
   }
 
   private getUrlString(productTypeObject: any){
-    let replacedString = productTypeObject.name.replace(/[^a-zA-Z]/g, '%');
+    let replacedString = productTypeObject.name.replace(/[/ ]/g, '');
 
     return replacedString + '-' + productTypeObject.id;
   }
